a62068d02613a9a6a5e3ad5d70acfa80a392fe59
shmily <83432087@qq.com>
2019-03-31 23:15:25 +0800

    update

---
+++ springboot-log4j2/src/test/java/com/mm/Log4j2Test.java
@@ -0,0 +1,22 @@
+package com.mm;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Log4j2Test.class)
+public class Log4j2Test {
+
+    @Test
+    public void testLog(){
+        log.trace("I am trace log.");
+        log.debug("I am debug log.");
+        log.info("I am info log.");
+        log.warn("I am warn log.");
+        log.error("I am error log.");
+    }
+}
