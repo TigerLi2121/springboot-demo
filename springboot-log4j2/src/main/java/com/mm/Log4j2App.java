3cd078e73c93748ba1ad79b1c52c3f87dc5c5e30
lwl <83432087@qq.com>
2019-04-01 17:48:04 +0800

    update log4j2

---
+++ springboot-log4j2/src/main/java/com/mm/Log4j2App.java
@@ -0,0 +1,17 @@
+package com.mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * log4j2日志
+ * @author lwl
+ * @date 2019/4/1
+ */
+@SpringBootApplication
+public class Log4j2App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Log4j2App.class, args);
+    }
+}
