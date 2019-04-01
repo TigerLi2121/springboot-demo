dd37000bf20389da71c0ddf243856de706840a46
lwl <83432087@qq.com>
2019-04-01 14:39:28 +0800

    add log4j2

--- springboot-log4j2/src/test/java/com/mm/Log4j2Test.java
+++ springboot-log4j2/src/test/java/com/mm/Log4j2Test.java
@@ -7,8 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @Slf4j
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = Log4j2Test.class)
+@RunWith(SpringRunner.class)
 public class Log4j2Test {
 
     @Test
