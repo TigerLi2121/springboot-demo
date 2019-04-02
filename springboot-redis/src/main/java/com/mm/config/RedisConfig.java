78b6f2e2338fec9331949f4075e2a91762c60570
shmily <83432087@qq.com>
2019-04-02 23:58:38 +0800

    update redis

--- springboot-redis/src/main/java/com/mm/config/RedisConfig.java
+++ springboot-redis/src/main/java/com/mm/config/RedisConfig.java
@@ -3,19 +3,15 @@ package com.mm.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-
-import java.lang.reflect.Method;
 
 /**
  * @author: Shmily
@@ -26,11 +22,12 @@ import java.lang.reflect.Method;
 public class RedisConfig extends CachingConfigurerSupport {
     /**
      * 此方法将会根据类名+方法名+所有参数的值生成唯一的一个key
+     *
      * @return
      */
     @Bean
     @Override
-    public KeyGenerator keyGenerator(){
+    public KeyGenerator keyGenerator() {
         return (target, method, params) -> {
             StringBuilder sb = new StringBuilder();
             sb.append(target.getClass().getName());
