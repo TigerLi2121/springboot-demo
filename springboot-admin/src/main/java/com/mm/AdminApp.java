2fd1784e0d5367959fadcdb0e401c14c3d0b7a41
lwl <83432087@qq.com>
2019-04-11 16:30:41 +0800

    update actuator

--- springboot-admin/src/main/java/com/mm/AdminApp.java
+++ springboot-admin/src/main/java/com/mm/AdminApp.java
@@ -3,13 +3,13 @@ package com.mm;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * 监控
  */
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableAdminServer
 public class AdminApp {
     public static void main(String[] args) {
