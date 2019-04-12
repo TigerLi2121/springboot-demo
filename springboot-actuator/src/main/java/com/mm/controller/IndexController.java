b59894f449643b27e8b2bfc31e3f921663e560c1
lwl <83432087@qq.com>
2019-04-12 09:32:14 +0800

    解决 springboot-admin 运行tomcat下异常

--- springboot-actuator/src/main/java/com/mm/controller/IndexController.java
+++ springboot-actuator/src/main/java/com/mm/controller/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
-    @GetMapping("/index")
+    @GetMapping("/")
     public String index() {
         return "Hello World!";
     }
