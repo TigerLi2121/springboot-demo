3acb6171d3d815bf74fa579c47e902c6dfa88008
lwl <83432087@qq.com>
2019-04-17 17:54:45 +0800

    update jdbc

---
+++ springboot-redis/src/test/java/com/mm/SetTest.java
@@ -0,0 +1,32 @@
+package com.mm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * set 测试
+ *
+ * @author lwl
+ * @date 2019/4/16
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SetTest {
+
+    public final String SINGLE_DEMAND_FLAG = "single-demand-flag";
+
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void save() {
+        System.out.println(redisTemplate.opsForSet().add(SINGLE_DEMAND_FLAG, "11"));
+        System.out.println(redisTemplate.opsForSet().add(SINGLE_DEMAND_FLAG, "22"));
+        System.out.println(redisTemplate.opsForSet().randomMember(SINGLE_DEMAND_FLAG));
+    }
+}
