586fa688cce9394119a84536c4821077a2d5e60d
liwulin <83432087@qq.com>
2019-12-25 20:46:51 +0800

    update websocket

---
+++ springboot-netty-websocket/src/main/java/com/mm/NettyWebSocketServer.java
@@ -0,0 +1,54 @@
+package com.mm;
+
+import com.mm.handler.WebSocketChannelHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.*;
+
+@Slf4j
+@Configuration
+public class NettyWebSocketServer implements CommandLineRunner {
+
+    static ExecutorService es = new ThreadPoolExecutor(1, 1,0L, TimeUnit.MILLISECONDS,
+            new LinkedBlockingQueue<Runnable>());
+
+    public static void main(String[] args) {
+        NettyWebSocketServer server = new NettyWebSocketServer();
+        es.submit(()-> server.run());
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        es.submit(()-> run());
+    }
+
+    /**
+     * 开启服务端
+     */
+    private void run() {
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(eventLoopGroup, workGroup);
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.childHandler(new WebSocketChannelHandler());
+            log.info("服务端开启等待客户端连接..");
+            Channel channel = serverBootstrap.bind(8888).sync().channel();
+            channel.closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //退出程序
+            eventLoopGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+}
