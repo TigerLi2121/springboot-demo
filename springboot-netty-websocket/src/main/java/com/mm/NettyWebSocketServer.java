69e8f9364718fb3e41f0ed3eaba8975705bc8c2a
shmily <83432087@qq.com>
2019-12-26 20:17:59 +0800

    add thread pool name

--- springboot-netty-websocket/src/main/java/com/mm/NettyWebSocketServer.java
+++ springboot-netty-websocket/src/main/java/com/mm/NettyWebSocketServer.java
@@ -6,27 +6,31 @@ import io.netty.channel.Channel;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.concurrent.DefaultThreadFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Configuration
 public class NettyWebSocketServer implements CommandLineRunner {
-
-    static ExecutorService es = new ThreadPoolExecutor(1, 1,0L, TimeUnit.MILLISECONDS,
-            new LinkedBlockingQueue<Runnable>());
+    static ExecutorService es = new ThreadPoolExecutor(1, 1, 0L,
+            TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(),
+            new DefaultThreadFactory("netty-thread"));
 
     public static void main(String[] args) {
         NettyWebSocketServer server = new NettyWebSocketServer();
-        es.submit(()-> server.run());
+        es.submit(() -> server.run());
     }
 
     @Override
     public void run(String... args) throws Exception {
-        es.submit(()-> run());
+        es.submit(() -> run());
     }
 
     /**
