92dcbc4460cf064eeb9f71f19b02622c07013997
liwulin <83432087@qq.com>
2021-02-20 13:55:45 +0800

    update api exception

--- springboot-api/src/main/java/com/mm/api/exception/GException.java
+++ springboot-api/src/main/java/com/mm/api/exception/GException.java
@@ -5,7 +5,14 @@ import lombok.Getter;
 @Getter
 public class GException extends RuntimeException {
     private String msg;
+    private int code = 500;
+
     public GException(String msg) {
+        this.msg = msg;
+    }
+
+    public GException(int code, String msg) {
+        this.code = code;
         this.msg = msg;
     }
 }
--- springboot-api/src/main/java/com/mm/api/exception/GExceptionHandler.java
+++ springboot-api/src/main/java/com/mm/api/exception/GExceptionHandler.java
@@ -19,27 +19,32 @@ import java.util.stream.Collectors;
 @RestControllerAdvice
 public class GExceptionHandler {
 
-    @ExceptionHandler(value = Exception.class)
-    public R handle(Exception e) {
-        if (e instanceof GException) {
-            GException ge = (GException) e;
-            return R.error(ge.getMsg());
-        } else if (e instanceof BindException) {
-            BindException be = (BindException) e;
-            String msg = be.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
-            return R.error(100, msg);
-        } else if (e instanceof MethodArgumentNotValidException) {
-            MethodArgumentNotValidException me = (MethodArgumentNotValidException) e;
-            String msg = me.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
-            return R.error(100, msg);
-        } else if (e instanceof ConstraintViolationException) {
-            ConstraintViolationException ce = (ConstraintViolationException) e;
-            String msg = ce.getConstraintViolations().stream().map(m -> m.getMessage())
-                    .collect(Collectors.toList()).get(0);
-            return R.error(100, msg);
-        } else {
-            log.error("【系统异常】:", e);
-            return R.error("系统异常");
-        }
+    @ExceptionHandler(BindException.class)
+    public R handleException(BindException e) {
+        String msg = e.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        return R.error(100, msg);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public R handleException(MethodArgumentNotValidException e) {
+        String msg = e.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        return R.error(100, msg);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public R handleException(ConstraintViolationException e) {
+        String msg = e.getConstraintViolations().stream().map(m -> m.getMessage()).collect(Collectors.toList()).get(0);
+        return R.error(100, msg);
+    }
+
+    @ExceptionHandler(GException.class)
+    public R handleException(GException e) {
+        return R.error(e.getCode(), e.getMsg());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public R handleException(Exception e) {
+        log.error("【系统异常】:", e);
+        return R.error("系统异常");
     }
 }
